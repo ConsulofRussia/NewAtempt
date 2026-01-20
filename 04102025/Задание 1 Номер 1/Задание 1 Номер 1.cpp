fe7cb03310dd2822347cfb4367b048cd6d3b6b49
ConsulofRussia <maksgal07@gmail.com>
2026-01-20 21:31:12 +0300

    04.10.2025

---
+++ 04102025/Задание 1 Номер 1/Задание 1 Номер 1.cpp
@@ -0,0 +1,49 @@
+﻿// Задание 1 Номер 1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int rows, cols, startValue;
+
+    cout << "Введите количество строк: ";
+    cin >> rows;
+    cout << "Введите количество столбцов: ";
+    cin >> cols;
+    cout << "Введите начальное значение: ";
+    cin >> startValue;
+
+    int** array = new int* [rows];
+    for (int i = 0; i < rows; i++) {
+        array[i] = new int[cols];
+    }
+
+    int current = startValue;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            array[i][j] = current;
+            current *= 2;
+        }
+    }
+
+    cout << "\nПолученный массив:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << array[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        delete[] array[i];
+    }
+    delete[] array;
+
+    return 0;
+}
---
+++ 04102025/Задание 1 Номер 3/Задание 1 Номер 3.cpp
@@ -0,0 +1,89 @@
+﻿// Задание 1 Номер 3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int** createArray(int r, int c) {
+    int** arr = new int* [r];
+    for (int i = 0; i < r; i++) arr[i] = new int[c];
+    return arr;
+}
+
+void fillRandom(int** arr, int r, int c) {
+    srand(time(NULL));
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            arr[i][j] = rand() % 10;
+}
+
+void printArray(int** arr, int r, int c) {
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void shiftLeft(int** arr, int r, int c, int k) {
+    k %= c;
+    if (k == 0) return;
+
+    int* temp = new int[c];
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) temp[j] = arr[i][j];
+        for (int j = 0; j < c; j++) arr[i][j] = temp[(j + k) % c];
+    }
+
+    delete[] temp;
+}
+
+void shiftRight(int** arr, int r, int c, int k) {
+    k %= c;
+    if (k == 0) return;
+
+    int* temp = new int[c];
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) temp[j] = arr[i][j];
+        for (int j = 0; j < c; j++) arr[i][j] = temp[(j - k + c) % c];
+    }
+
+    delete[] temp;
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int rows = 3, cols = 4, shift, dir;
+
+    int** arr = createArray(rows, cols);
+    fillRandom(arr, rows, cols);
+
+    cout << "Исходный массив:\n";
+    printArray(arr, rows, cols);
+
+    cout << "Сдвиг (1-влево, 2-вправо): ";
+    cin >> dir;
+    cout << "На сколько: ";
+    cin >> shift;
+
+    if (dir == 1) shiftLeft(arr, rows, cols, shift);
+    else if (dir == 2) shiftRight(arr, rows, cols, shift);
+
+    cout << "\nПосле сдвига:\n";
+    printArray(arr, rows, cols);
+
+    for (int i = 0; i < rows; i++) delete[] arr[i];
+    delete[] arr;
+
+    return 0;
+}
