b70ea7160ad9bb4bc799aeeb54251e5c338b6abf
ConsulofRussia <maksgal07@gmail.com>
2026-01-21 14:10:37 +0300

    20.11.2025

---
+++ 20112025/Задание 1 Номер 1/Задание 1 Номер 1.cpp
@@ -0,0 +1,75 @@
+﻿// Задание 1 Номер 1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int** addColumn(int** arr, int rows, int& cols, int pos) {
+
+    int** newArr = new int* [rows];
+    for (int i = 0; i < rows; i++) {
+        newArr[i] = new int[cols + 1];
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0, k = 0; j < cols + 1; j++, k++) {
+            if (j == pos) {
+                newArr[i][j] = 0;
+                k--;
+            }
+            else {
+                newArr[i][j] = arr[i][k];
+            }
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        delete[] arr[i];
+    }
+    delete[] arr;
+
+    cols++;
+    return newArr;
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int rows = 3, cols = 3;
+
+    int** matrix = new int* [rows];
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = new int[cols];
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = i * 10 + j;
+        }
+    }
+
+    cout << "Исходная матрица:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    matrix = addColumn(matrix, rows, cols, 1);
+
+    cout << "\nПосле добавления столбца:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < rows; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
+    return 0;
+}
---
+++ 20112025/Задание 1 Номер 3/Задание 1 Номер 3.cpp
@@ -0,0 +1,112 @@
+﻿// Задание 1 Номер 3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+void shiftRowsDown(int** arr, int rows, int cols, int shifts) {
+    shifts %= rows;
+    if (shifts == 0) return;
+
+    int* temp = new int[cols];
+
+    for (int s = 0; s < shifts; s++) {
+
+        for (int j = 0; j < cols; j++) {
+            temp[j] = arr[rows - 1][j];
+        }
+
+        for (int i = rows - 1; i > 0; i--) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = arr[i - 1][j];
+            }
+        }
+
+        for (int j = 0; j < cols; j++) {
+            arr[0][j] = temp[j];
+        }
+    }
+
+    delete[] temp;
+}
+
+void shiftColumnsRight(int** arr, int rows, int cols, int shifts) {
+    shifts %= cols;
+    if (shifts == 0) return;
+
+    int* temp = new int[rows];
+
+    for (int s = 0; s < shifts; s++) {
+        for (int i = 0; i < rows; i++) {
+            temp[i] = arr[i][cols - 1];
+        }
+        for (int j = cols - 1; j > 0; j--) {
+            for (int i = 0; i < rows; i++) {
+                arr[i][j] = arr[i][j - 1];
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            arr[i][0] = temp[i];
+        }
+    }
+
+    delete[] temp;
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int rows, cols;
+
+    cout << "Введите количество строк: ";
+    cin >> rows;
+    cout << "Введите количество столбцов: ";
+    cin >> cols;
+
+    int** matrix = new int* [rows];
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = new int[cols];
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = i * 10 + j;
+        }
+    }
+
+    cout << "\nИсходная матрица:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    int choice, shifts;
+    cout << "\n1. Сдвиг строк вниз\n2. Сдвиг столбцов вправо\nВыберите операцию: ";
+    cin >> choice;
+    cout << "Сколько раз сдвинуть: ";
+    cin >> shifts;
+
+    if (choice == 1) {
+        shiftRowsDown(matrix, rows, cols, shifts);
+    }
+    else if (choice == 2) {
+        shiftColumnsRight(matrix, rows, cols, shifts);
+    }
+
+    cout << "\nМатрица после сдвига:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < rows; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
+    return 0;
+}
